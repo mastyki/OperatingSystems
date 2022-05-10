522a62638c217e5b5e62c0d4265e23f904c4bcf3
Liza Mastykina <liza.mastykina@gmail.com>
2022-05-10 22:13:34 +0300

    Create process

--- lab4_C98/main.cpp
+++ lab4_C98/main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<Windows.h>
+#include<vector>
 
+using namespace std;
+
+HANDLE start_process(wstring command_line) {
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+
+    CloseHandle(pi.hThread);
+    return pi.hProcess;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
